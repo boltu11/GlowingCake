c0381c3caaa214c0c55d40ac5f2dde7a1ebf6844
navjot <navjotbhatti22@gmail.com>
2020-08-31 19:57:50 -0700

    readd

--- app/src/main/java/com/example/glowingcake/MainActivity.java
+++ app/src/main/java/com/example/glowingcake/MainActivity.java
@@ -4,32 +4,117 @@ import android.os.Bundle;
 import android.util.Log;
 
 
+import android.view.SurfaceView;
+import android.view.WindowManager;
 import android.widget.TextView;
+
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraActivity;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.CameraActivity;
+import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
 import org.opencv.core.Mat;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Collections;
+import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
 
+public class MainActivity extends CameraActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
+    private static final String TAG = "OCVSample::Activity";
+    private CameraBridgeViewBase mOpenCvCameraView;
     // Used to load the 'native-lib' library on application startup.
     static {
         System.loadLibrary("native-lib");
     }
 
+    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status) {
+                case LoaderCallbackInterface.SUCCESS:
+                {
+                    Log.i(TAG, "OpenCV loaded successfully");
+                    mOpenCvCameraView.enableView();
+                } break;
+                default:
+                {
+                    super.onManagerConnected(status);
+                } break;
+            }
+        }
+    };
+    public MainActivity() {
+        Log.i(TAG, "Instantiated new " + this.getClass());
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
         setContentView(R.layout.activity_main);
 
-        // Example of a call to a native method
-        TextView tv = findViewById(R.id.sample_text);
 
-        ble();
-        tv.setText(stringFromJNI());
+        mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.camera_view);
+        mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
+
+        mOpenCvCameraView.setCvCameraViewListener(this);
     }
 
-    public void ble(){
-        Log.i("RE","SAMTAM");
+    @Override
+    public void onPause()
+    {
+        super.onPause();
+        if (mOpenCvCameraView != null)
+            mOpenCvCameraView.disableView();
+    }
+
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        if (!OpenCVLoader.initDebug()) {
+            Log.d(TAG, "Internal OpenCV library not found. Using OpenCV Manager for initialization");
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_0_0, this, mLoaderCallback);
+        } else {
+            Log.d(TAG, "OpenCV library found inside package. Using it!");
+            mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+        }
+    }
+
+    @Override
+    protected List<? extends CameraBridgeViewBase> getCameraViewList() {
+        return Collections.singletonList(mOpenCvCameraView);
+    }
+
+    public void onDestroy() {
+        super.onDestroy();
+        if (mOpenCvCameraView != null)
+            mOpenCvCameraView.disableView();
+    }
+
+
+    public void onCameraViewStarted(int width, int height){
+
+    }
+
+
+    public void onCameraViewStopped(){
+
+    }
+
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame input) {
+        final int view_mode = mViewMode;
+
+        switch( view_mode){
+            case VIEW_MODE_
+        }
+
+        return input.rgba();
     }
 
     /**
